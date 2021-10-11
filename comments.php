1601c014cc002c282a3b1f67e69fa731005e16f9
bejibay <58639255+bejibay@users.noreply.github.com>
2021-10-11 11:51:16 +0100

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,1 +1,28 @@
-Comments.php
+
+?php 
+$action=isset($ction)?$_GET['action']:"";
+?>
+<?php 
+if($action)){
+Comments::getComments($action);
+foreach($row as $row){}
+if($_POST['savecomments']){
+$comments=new Comments();
+$comments->storeForm($_POST);
+$comments->insertComments($action);
+Comments::getComments($action);
+foreach($row as $row){
+echo "<ul>";
+}
+}
+}
+?>
+
+
+
+
+
+
+
+
+
