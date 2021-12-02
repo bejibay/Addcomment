a1b57ef10b7a4834551a115f516f617174f68e99
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-02 08:38:35 +0100

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,5 +1,23 @@
 
 ?php 
+Class Comments{
+public $id = null;
+public $pageurl = null;
+public $name = null;
+public $email = null;
+public $website = null;
+public $comment = null;
+public function __construct($data=array()){
+if(isset($data['id']))$this->id=int($data['id']);
+if(isset($data['name']))$this->name=$data['name'];
+if(isset($data['email']))$this->email=$data['email'];
+if(isset($data['pageurl']))$this->pageurl=$data['pageurl'];
+if(isset($data['pageurl']))$this->pageurl=$data['pageurl'];
+if(isset($data['pageurl']))$this->pageurl=$data['pageurl'];
+
+}
+}
+
 $action=isset($ction)?$_GET['action']:"";
 ?>
 <?php 
