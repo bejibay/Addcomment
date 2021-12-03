11d450fd746961011a946b61e79594be58cb0715
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-04 00:27:48 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
-//this contains the configuration for the dstabase
+
 
 <?php
-define( "DB_DSN", "mysql:host=localhost;dbname=cms" );
+//the configuration for the dstabase
+//template and class paths
+define( "DB_DSN", "mysql:host=localhost;dbname=mydb" );
 define( "DB_USERNAME", "username" );
 define( "DB_PASSWORD", "password" );
 define( "CLASS_PATH", "classes" );
 define( "TEMPLATE_PATH", "templates" );
 require( CLASS_PATH . "/Article.php" );
+?>
 
 
