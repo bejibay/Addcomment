89002a7f7d16b46aa94ed063c7709b70b2d9cffc
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-03 12:31:43 +0100

    Update selectcomments.php

--- selectcomments.php
+++ selectcomments.php
@@ -1,12 +1,9 @@
-//this script us to select comments for a page from
+//this script is to select comments for a page from
 Database
-<?php
-<div class ="formcomments"></div>
-?>
+
 <?php 
 $action=isset($ction)?$_GET['action']:"";
 if($action)){
-if($_POST['savecomments']){
 $comments=new Comments();
 $comments->selectsComments($action);}
 
