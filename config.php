1a202b612976fc5ea9b897d3628f448cf55b83f7
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-04 00:40:40 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,13 +2,11 @@
 
 <?php
 //the configuration for the dstabase
-//template and class paths
+//require the comments class
 define( "DB_DSN", "mysql:host=localhost;dbname=mydb" );
 define( "DB_USERNAME", "username" );
 define( "DB_PASSWORD", "password" );
-define( "CLASS_PATH", "classes" );
-define( "TEMPLATE_PATH", "templates" );
-require( CLASS_PATH . "/Article.php" );
+require("commentsclass.php" );
 ?>
 
 
