418fa38c7becd25408eb01484e652b78bf5b89fb
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-03 10:42:10 +0100

    Update selectcomments.php

--- selectcomments.php
+++ selectcomments.php
@@ -8,9 +8,7 @@ $action=isset($ction)?$_GET['action']:"";
 if($action)){
 if($_POST['savecomments']){
 $comments=new Comments();
-$comments->storeFormData($_POST);
-$comments->insertComments($action);}
-if($_POST['cancelcomments']){include 'commentsform.php';}
-else{include 'commentsform.php';}
+$comments->selectsComments($action);}
+
 }
 ?>
