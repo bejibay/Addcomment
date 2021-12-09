01082ef479411a661679feca821e5b8958e4e3ef
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-09 22:45:30 +0100

    Update selectcomments.php

--- selectcomments.php
+++ selectcomments.php
@@ -9,3 +9,17 @@ $comments->selectsComments($action);}
 
 }
 ?>
+<?php require( "config.php");?>
+<?php 
+$action=isset($ction)?$_GET['action']:"";
+if($action)){
+$comments=new Comments;
+$comments->selectComments($action);
+foreach($row as $value){
+echo "<ul>";
+echo "<li>$row['name']</li>";
+echo "<li>$row['comment']</li>";
+echo "</ul>";
+}
+}
+?>
