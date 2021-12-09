ed7c9de87aa29a72f19cea19341585eeb179e7ae
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-09 22:32:38 +0100

    Update postcomments.php

--- postcomments.php
+++ postcomments.php
@@ -6,8 +6,8 @@ $comments=new Comments;
 $comments->selectComments($action);
 foreach($row as $value){
 echo "<ul>";
-echo "<li>$row['name']</li>;
-echo "<li>$row['comment']</li>;
+echo "<li>$row['name']</li>";
+echo "<li>$row['comment']</li>";
 echo "</ul>";
 }
 }
