29ae12c4f44207551055a6f9627d63459a28e28d
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-09 22:51:46 +0100

    Update selectcomments.php

--- selectcomments.php
+++ selectcomments.php
@@ -1,15 +1,4 @@
 <?php require( "config.php");?>
-
-<?php 
-//to select comments for a page from database
-$action=isset($ction)?$_GET['action']:"";
-if($action)){
-$comments=new Comments();
-$comments->selectsComments($action);}
-
-}
-?>
-<?php require( "config.php");?>
 <?php 
 $action=isset($ction)?$_GET['action']:"";
 if($action)){
