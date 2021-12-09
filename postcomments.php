239e7c6aa142ebaae2f68032ff0cd7efbe9c9d59
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-09 23:06:24 +0100

    Update postcomments.php

--- postcomments.php
+++ postcomments.php
@@ -3,13 +3,8 @@
 $action=isset($ction)?$_GET['action']:"";
 if($action)){
 $comments=new Comments;
-$comments->selectComments($action);
-foreach($row as $value){
-echo "<ul>";
-echo "<li>$row['name']</li>";
-echo "<li>$row['comment']</li>";
-echo "</ul>";
-}
+$comments->storeFormValues();
+$comments->postComments();
 }
 ?>
 
