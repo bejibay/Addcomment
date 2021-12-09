e8761dac977ac9ef4271fb6061111b7fcf797063
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-09 22:25:09 +0100

    Create postcomments.php

--- postcomments.php
+++ postcomments.php
@@ -2,9 +2,14 @@
 <?php 
 $action=isset($ction)?$_GET['action']:"";
 if($action)){
-$comments=new Comments();
-$comments->selectsComments($action);}
-
+$comments=new Comments;
+$comments->selectComments($action);}
+ foreach($row as $value){
+echo "<ul>";
+echo "<li>$row['name']</li>;
+echo "<li>$row['comment']</li>;
+echo "</ul>";
+}
 }
 ?>
 
