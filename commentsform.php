7d945caf4039f7a1e919542ed8db8bda15c11cdb
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-23 02:03:29 +0100

    Create commentsform.php

---
+++ commentsform.php
@@ -0,0 +1,16 @@
+<form method ="post" action ="insertcomments.php">
+<div>
+<label for="name">Name<label/>
+<input type ="text" name ="name">
+<label for="email">Email<label/>
+<input type ="text" name ="email">
+<label for="website">Website<label/>
+<input type ="text" name ="website" >
+<label for="comments">Comments<label/>
+<textarea name ="comments"></textarea>
+<input type ="hidden" name ="ip" value="<?php echo $_SERVER['REMOTE_ADD"];?>"> 
+<input type ="date" name="pubdate" >
+</div>
+<input type ="submit" name ="submit" value ="Submit Comments">
+<input type ="reset" name ="cancel" value ="Cancel">
+</form>
