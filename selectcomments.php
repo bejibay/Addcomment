02b1059dc1a537dee9f095c2c4359ef539de07ec
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-23 01:49:50 +0100

    Update selectcomments.php

--- selectcomments.php
+++ selectcomments.php
@@ -1,13 +1,13 @@
 <?php require( "config.php");?>
 <?php 
 $action=isset($ction)?$_GET['action']:"";
-if($action)){
+if($action){
 $comments=new Comments;
 $comments->selectComments($action);
 foreach($row as $value){
 echo "<ul>";
-echo "<li>$row['name']</li>";
-echo "<li>$row['comment']</li>";
+echo "<li>".$value['name']."</li>";
+echo "<li>".$value['comment']."</li>";
 echo "</ul>";
 }
 }
