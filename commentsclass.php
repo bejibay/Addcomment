36b106957ac424d34c1054579b96898106bfcb98
bejibay <58639255+bejibay@users.noreply.github.com>
2022-02-15 11:23:52 +0100

    Update commentsclass.php

--- commentsclass.php
+++ commentsclass.php
@@ -6,7 +6,7 @@ public $pageurl = null;
 public $name = null;
 public $email = null;
 public $website = null;
-public $comment = "";
+public $comment = null;
 public $ip     = "";
 public function __construct($data=array()){
 global $message;
@@ -17,7 +17,7 @@ else{$message = "Your name cannot be empty";}
 if(isset($data['email']) && filter_var($data['email'],FILTER_VALIDATE_EMAIL)){$this->email=$data['email'];}
 else{$message="invalid email";}
 if(isset($data['website']) && filter_var($data['website'],FILTER_VALIDATE_URL)){$this->website=$data['website'];}
-else{$message="invalid website";}}
+else{$message="invalid website";}
 if(isset($data['comment'])){$this->comment=$data['comment'];}
 else{$message= " Comments cannot be empty";}
 }
@@ -31,19 +31,19 @@ list($y,$m,$d)=$pubdate;
 $this->pubdate = mktime(0,0,0,$m,$d,$y);
 }
 }
-
+}
 public function insertComments(){
 $conn = new PDO(DB_DSN,DB_USERNAME,DB_PASSWORD);
 $sql = " INSERT INTO commentstable(pageurl,name, email,website,comment,ip,pubdate)
 VALUES(:pageurl,:name,:website,:comment,:ip,:pubdate)";
 $stm = $conn->prepare($sql);
-$stm-bindValue(":pageurl",$this->pageurl,PDO::PARAM_STR);
-$stm-bindValue(":name",$this->name,PDO::PARAM_STR);
-$stm-bindValue(":email",$this->email,PDO::PARAM_STR);
-$stm-bindValue(":website",$this->website,PDO::PARAM_STR);
-$stm-bindValue(":comment",$this->comment,PDO::PARAM_STR);
-$stm-bindValue(":ip",$this->ip,PDO::PARAM_INT);
-$stm-bindValue(":pubdate",$this->pubdate,PDO::PARAM_INT)
+$stm->bindValue(":pageurl",$this->pageurl,PDO::PARAM_STR);
+$stm->bindValue(":name",$this->name,PDO::PARAM_STR);
+$stm->bindValue(":email",$this->email,PDO::PARAM_STR);
+$stm->bindValue(":website",$this->website,PDO::PARAM_STR);
+$stm->bindValue(":comment",$this->comment,PDO::PARAM_STR);
+$stm->bindValue(":ip",$this->ip,PDO::PARAM_INT);
+$stm->bindValue(":pubdate",$this->pubdate,PDO::PARAM_INT);
 $stm->execute();
 $conn->null;
 }
@@ -60,14 +60,3 @@ if($row) return new Comments($row);
 }
 }
 ?>
-
-
-
-
-
-
-
-
-
-
-
