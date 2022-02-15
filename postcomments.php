ab3e0696d5fcc844b858b53b74eda10c162248ca
bejibay <58639255+bejibay@users.noreply.github.com>
2022-02-15 16:06:25 +0100

    Update postcomments.php

--- postcomments.php
+++ postcomments.php
@@ -1,13 +1,13 @@
-<?php require( "config.php");?>
-<?php 
-$action=isset($ction)?$_GET['action']:"";
-if($action){
+<?php require "config.php" ;
+if(isset($_GET['pageurl'])){
+if(isset($_POST['post comments'])){
 $comments=new Comments;
 $comments->storeFormData($_POST);
 $comments->postComments();
+<?php require "commentsform.php" ;
+}
 }
 ?>
-<?php include "commentsform.php";?>
 
 
 
